22709cec8f6182c19748bea3143f8229dc2f6348
Sumit.p <sumit.p@peppefry.com>
2025-07-29 17:14:02 +0530

    Variables and Data types Exercise solution 2

--- src/java_basics/VariablesAndDatatypes_Exercise.java
+++ src/java_basics/VariablesAndDatatypes_Exercise.java
@@ -3,23 +3,41 @@ package java_basics;
 public class VariablesAndDatatypes_Exercise {
 
 	public static void main(String[] args) {
-		/*Exercises
-		Write a Java program to store your:
-
-		Name (String)
-
-		Age (int)
-
-		Salary (double)
-
-		QA status (boolean)
-		And print them in one line*/
+		/*
+		 * Exercises Write a Java program to store your: Exercise-1 Name (String)
+		 * 
+		 * Age (int)
+		 * 
+		 * Salary (double)
+		 * 
+		 * QA status (boolean) And print them in one line
+		 */
 		String name = "Sumit";
 		int age = 28;
 		double salary = 50000.00;
 		boolean qaStatus = true;
-		System.out.println("My name is " + name + ", My age is " + age + ", My salary is " + salary + ". am I a QA Engineer? " + qaStatus);
+		System.out.println("My name is " + name + ", My age is " + age + ", My salary is " + salary
+				+ ". am I a QA Engineer? " + qaStatus);
 
+		/* Change the value of variables and print again. */
+		name = "QA Sumit";
+		age = 30;
+		salary = 55000.00;
+		qaStatus = false;
+		System.out.println("My name is " + name + ", My age is " + age + ", My salary is " + salary
+				+ ". am I a QA Engineer? " + qaStatus);
+
+		/*
+		 * Take two numbers and print: Sum Difference Product
+		 */
+		
+		int num1 = 20;
+		int num2 = 10;
+		int sum = num1 + num2, diff = num1 - num2, product = num1 * num2;
+		
+		System.out.println("The sum of numbers is " + sum);
+		System.out.println("The difference between the numbers is " + diff);
+		System.out.println("The product of numbers is " + product);
 	}
 
 }
