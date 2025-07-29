734ef43d620741af4179237402480ee065655d0e
Sumit.p <sumit.p@peppefry.com>
2025-07-29 14:59:43 +0530

    Variables and Data types Exercise solution

---
+++ src/java_basics/VariablesAndDatatypes_Exercise.java
@@ -0,0 +1,25 @@
+package java_basics;
+
+public class VariablesAndDatatypes_Exercise {
+
+	public static void main(String[] args) {
+		/*Exercises
+		Write a Java program to store your:
+
+		Name (String)
+
+		Age (int)
+
+		Salary (double)
+
+		QA status (boolean)
+		And print them in one line*/
+		String name = "Sumit";
+		int age = 28;
+		double salary = 50000.00;
+		boolean qaStatus = true;
+		System.out.println("My name is " + name + ", My age is " + age + ", My salary is " + salary + ". am I a QA Engineer? " + qaStatus);
+
+	}
+
+}
