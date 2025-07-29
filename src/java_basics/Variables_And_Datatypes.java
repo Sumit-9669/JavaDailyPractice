1bbe10acd132ca50e838ef875c6d9971377ff48d
Sumit.p <sumit.p@peppefry.com>
2025-07-29 12:24:48 +0530

    Variables and Data types

---
+++ src/java_basics/Variables_And_Datatypes.java
@@ -0,0 +1,46 @@
+package java_basics;
+
+public class Variables_And_Datatypes {
+
+	public static void main(String[] args) {
+		// Java has primitive data types: int, double, char, float, boolean, long, short, byte
+		/*Variables: containers for data types.
+		 In simple words, Variables are used to store the data types*/
+		
+		int age = 28;
+		double decimal_number = 445.34;
+		char grade = 'A';
+		boolean isQA = true;
+		
+		System.out.println("The age is " + age);
+		System.out.println("the decimal number is " + decimal_number);
+		System.out.println("The secured grade is " + grade);
+		System.out.println("is a QA? " + isQA);
+
+	}
+
+}
+
+/*Exercises
+Write a Java program to store your:
+
+Name (String)
+
+Age (int)
+
+Salary (double)
+
+QA status (boolean)
+And print them in one line
+
+
+(Extra challenge)
+Take two numbers and print:
+
+Sum
+
+Difference
+
+Product
+
+*/
