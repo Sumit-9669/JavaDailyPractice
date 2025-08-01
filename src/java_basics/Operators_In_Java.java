8e151c9cdb3bc6f897067065a4350ad3cb0d006a
Sumit.p <sumitp9669@gmail.com>
2025-08-01 17:55:40 +0530

    Modularised operators code in java 2

--- src/java_basics/Operators_In_Java.java
+++ src/java_basics/Operators_In_Java.java
@@ -10,11 +10,11 @@ public class Operators_In_Java {
 
 		obj.arithmeticOperators();
 		obj.relationalOperators();
-		obj.logicalOperators(); 
-		/*obj.assignmentOperators();
-		 obj.incrementDecrementOperator(); 
-		 obj.ternaryOperator();
-		 */
+		obj.logicalOperators();
+		obj.assignmentOperators();
+		obj.incrementDecrementOperator(); 
+		obj.ternaryOperator();
+		 
 	}
 
 	public void arithmeticOperators() {
@@ -49,12 +49,15 @@ public class Operators_In_Java {
 		System.out.println(num1 + " <= " + num2 + " : " + (num1 <= num2));
 
 	}
-	
+
 	public void logicalOperators() {
-		
-		/* Logical Operators: && (AND), || (OR), ! (NOT)
-		 * For this to understand you need to know the truth table in digital electronics for AND gate, OR gate & NOT gate*/
-		
+
+		/*
+		 * Logical Operators: && (AND), || (OR), ! (NOT) For this to understand you need
+		 * to know the truth table in digital electronics for AND gate, OR gate & NOT
+		 * gate
+		 */
+
 		boolean isQA = true;
 		boolean knowsJava = false;
 		System.out.println("\n========== Logical Operators ==========");
@@ -62,6 +65,50 @@ public class Operators_In_Java {
 		System.out.println("isQA || knowsJava : " + (isQA || knowsJava));
 		System.out.println("!isQA : " + (!isQA));
 		System.out.println("!knowsJava : " + (!knowsJava));
+
+	}
+
+	public void assignmentOperators() {
+
+		/* Assignment Operators: =, +=, -=, *=, /= */
+
+		System.out.println("\n=== Assignment Operators ===");
+		int a = num1;
+		System.out.println("Initial value of a: " + a); // value of num1 is now assigned to variable 'a'
+
+		a += num2;
+		System.out.println("a += num2 : " + a);
+		a -= num2;
+		System.out.println("a -= num2 : " + a);
+		a *= num2;
+		System.out.println("a *= num2 : " + a);
+		a /= num2;
+		System.out.println("a /= num2 : " + a);
+
+	}
+
+	public void incrementDecrementOperator() {
+		
+		/* Increment / Decrement Operators:x++, ++x, x--, --x */
+		System.out.println("\n========== Increment / Decrement Operators ==========");
+		int x = num1;
+
+		System.out.println("Original x: " + x);
+        System.out.println("Post-increment (x++): " + (x++));
+        System.out.println("After post increment: " + x);
+        System.out.println("Pre-increment (++x): " + (++x));
+        System.out.println("Post-decrement (x--): " + (x--));
+        System.out.println("After post-decrement: " + x);
+        System.out.println("Pre-decrement (--x): " + (--x));
+
+	}
+	
+	public void ternaryOperator() {
+		
+		/*condition ? value_if_true : value_if_false */		
+		System.out.println("\n========== Conditional (Ternary) Operator ==========");
+		String result = (num1 > num2) ? "num1 is greater" : "num2 is greater or equal";
+		System.out.println("result: " + result);
 		
 	}
 
