eb019a4a42a787cd7191eec27ccdbbaf1748eef6
Sumit.p <sumitp9669@gmail.com>
2025-08-07 15:01:27 +0530

    Nested if-else statement

---
+++ src/java_basics/Conditional_Statements_2.java
@@ -0,0 +1,29 @@
+package java_basics;
+
+import java.util.Scanner;
+
+public class Conditional_Statements_2 {
+
+	public static void main(String[] args) {
+		
+		//Nested if-else statements
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter your grades (0 to 100): ");
+
+		int grade = sc.nextInt();
+
+		if (grade >= 90) {
+			System.out.println("Grade A");
+		} else if (grade >= 70) {
+			System.out.println("Grade B");
+		} else if (grade >= 50) {
+			System.out.println("Grade C");
+		} else {
+			System.out.println("Grade D");
+		}
+		sc.close();
+
+	}
+
+}
