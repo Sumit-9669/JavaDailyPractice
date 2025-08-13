358af3b4df4f82a89001e457425d267d2cfd9af9
Sumit.p <sumitp9669@gmail.com>
2025-08-13 13:22:40 +0530

    Simple Calculator using Switch Statement

---
+++ src/java_basics/Simple_Calculator.java
@@ -0,0 +1,42 @@
+package java_basics;
+
+import java.util.Scanner;
+
+public class Simple_Calculator {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number: ");
+		double num1 = sc.nextDouble();
+
+		System.out.println("Enter second number: ");
+		double num2 = sc.nextDouble();
+
+		System.out.println("Choose Operation (+, -, *, /): ");
+		char op = sc.next().charAt(0);
+
+		switch (op) {
+		case '+':
+			System.out.println("sum = " + (num1 + num2));
+			break;
+
+		case '-':
+			System.out.println("Difference = " + (num1 - num2));
+			break;
+
+		case '*':
+			System.out.println("Product = " + (num1 * num2));
+			break;
+
+		case '/':
+			System.out.println("Division " + (num2 / num1));
+			break;
+
+		default:
+			System.out.println("Invalid Operator");
+		}
+		sc.close();
+
+	}
+
+}
