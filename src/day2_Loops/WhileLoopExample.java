06de844e034b49180f1de740c1a1516af69909ad
Sumit.p <sumitp9669@gmail.com>
2025-08-14 16:35:52 +0530

    Count down using while loop

--- src/day2_Loops/WhileLoopExample.java
+++ src/day2_Loops/WhileLoopExample.java
@@ -3,8 +3,14 @@ package day2_Loops;
 public class WhileLoopExample {
 
 	public static void main(String[] args) {
+		//Program to count down from 3 to 1 and then displays "Happy New Year!!" at the end:
 		
-
+		int countDown = 3;
+		while(countDown > 0) {
+			System.out.println(countDown);
+			countDown--;
+		}
+		System.out.println("Happy New Year 2025...!!!");
 	}
 
 }
