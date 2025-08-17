8e78fc45eaf5aa86c16f8b2d6b60f05814d90108
Sumit.p <sumitp9669@gmail.com>
2025-08-17 10:39:29 +0530

    do while loop

---
+++ src/day2_Loops/DoWhile_LoopExample.java
@@ -0,0 +1,15 @@
+package day2_Loops;
+
+public class DoWhile_LoopExample {
+
+	public static void main(String[] args) {
+		/*
+		 * do { // code block to be executed } while (condition);
+		 */
+		int i = 0;
+		do {
+			System.out.println(i);
+			i++;
+		} while (i <= 10);
+	}
+}
--- src/day2_Loops/WhileLoopExample.java
+++ src/day2_Loops/WhileLoopExample.java
@@ -3,10 +3,11 @@ package day2_Loops;
 public class WhileLoopExample {
 
 	public static void main(String[] args) {
-		//Program to count down from 3 to 1 and then displays "Happy New Year!!" at the end:
-		
+		// Program to count down from 3 to 1 and then displays "Happy New Year!!" at the
+		// end:
+
 		int countDown = 3;
-		while(countDown > 0) {
+		while (countDown > 0) {
 			System.out.println(countDown);
 			countDown--;
 		}
